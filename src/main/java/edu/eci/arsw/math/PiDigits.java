db7126f9178279f266eae767675066718b9558b8
unknown <laura.rojas-r@labinfo.is.escuelaing.edu.co>
2024-09-19 09:49:59 -0500

    Correcto

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -68,8 +68,9 @@ public class PiDigits {
             if (!vivo){
                 break;
             }
-            flag.set(true);
             Thread.sleep(5000);
+            flag.set(true);
+
 
 
             for (PiDigitsThread p : hilos){
@@ -103,67 +104,6 @@ public class PiDigits {
 
     }
 
-    /// <summary>
-    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
-    /// </summary>
-    /// <param name="m"></param>
-    /// <param name="n"></param>
-    /// <returns></returns>
-    public static double sum(int m, int n) {
-        double sum = 0;
-        int d = m;
-        int power = n;
 
-        while (true) {
-            double term;
-
-            if (power > 0) {
-                term = (double) hexExponentModulo(power, d) / d;
-            } else {
-                term = Math.pow(16, power) / d;
-                if (term < Epsilon) {
-                    break;
-                }
-            }
-
-            sum += term;
-            power--;
-            d += 8;
-        }
-
-        return sum;
-    }
-
-    /// <summary>
-    /// Return 16^p mod m.
-    /// </summary>
-    /// <param name="p"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    private static int hexExponentModulo(int p, int m) {
-        int power = 1;
-        while (power * 2 <= p) {
-            power *= 2;
-        }
-
-        int result = 1;
-
-        while (power > 0) {
-            if (p >= power) {
-                result *= 16;
-                result %= m;
-                p -= power;
-            }
-
-            power /= 2;
-
-            if (power > 0) {
-                result *= result;
-                result %= m;
-            }
-        }
-
-        return result;
-    }
 
 }
--- src/main/java/edu/eci/arsw/math/PiDigitsThread.java
+++ src/main/java/edu/eci/arsw/math/PiDigitsThread.java
@@ -11,6 +11,7 @@ public class PiDigitsThread extends Thread{
     private byte[] digits;
     private AtomicBoolean flag;
     private int cuantos = 0;
+    private static double Epsilon = 1e-17;
 
     public PiDigitsThread(int start, int count, AtomicBoolean flag) {
         this.start = start;
@@ -43,10 +44,10 @@ public class PiDigitsThread extends Thread{
 
             }
             if (i % DigitsPerSum == 0) {
-                sum = 4 * PiDigits.sum(1, start)
-                        - 2 * PiDigits.sum(4, start)
-                        - PiDigits.sum(5, start)
-                        - PiDigits.sum(6, start);
+                sum = 4 * sum(1, start)
+                        - 2 * sum(4, start)
+                        - sum(5, start)
+                        - sum(6, start);
 
                 start += DigitsPerSum;
             }
@@ -56,6 +57,69 @@ public class PiDigitsThread extends Thread{
             digits[i] = (byte) sum;
             cuantos ++;
         }
+    }
+
+    /// <summary>
+    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static double sum(int m, int n) {
+        double sum = 0;
+        int d = m;
+        int power = n;
+
+        while (true) {
+            double term;
+
+            if (power > 0) {
+                term = (double) hexExponentModulo(power, d) / d;
+            } else {
+                term = Math.pow(16, power) / d;
+                if (term < Epsilon) {
+                    break;
+                }
+            }
+
+            sum += term;
+            power--;
+            d += 8;
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    /// Return 16^p mod m.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    private static int hexExponentModulo(int p, int m) {
+        int power = 1;
+        while (power * 2 <= p) {
+            power *= 2;
+        }
+
+        int result = 1;
+
+        while (power > 0) {
+            if (p >= power) {
+                result *= 16;
+                result %= m;
+                p -= power;
+            }
+
+            power /= 2;
+
+            if (power > 0) {
+                result *= result;
+                result %= m;
+            }
+        }
+
+        return result;
     }
 }
 
